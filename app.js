fbe2c6beed7e9eae259ffab2694316ff6937332c
MichailVs <mcohotnik21@gmail.com>
2024-05-05 21:17:36 +0300

    2 version

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ function vec2(x, y) {
 }
 
 function z(cord) {
+
     return {x: cord[0] + cord[2], y: cord[1] + cord[2]}
+
 }
+
 function validateY(arr, i) {
     let temp = []
     arr.forEach(el => {
@@ -24,6 +27,7 @@ function validateY(arr, i) {
     })
     return temp
 }
+
 function validateX(arr, i) {
     let temp = []
     arr.forEach(el => {
@@ -61,52 +65,102 @@ function web() {
 }
 web()
 
-function cube(matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-        let z1 = z(matrix[i])
-        ctx.lineWidth = 1
-        ctx.strokeStyle = 'blue'
-        const zY = validateY(matrix, i)
-        const z2 = z(zY)
-        const zX = validateX(matrix, i)
-        const z3 = z(zX)
-        const zZ = validateZ(matrix, i)
-        const z4 = z(zZ)
-        ctx.beginPath()
-        ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
-        ctx.lineTo(z2.x + zY[0], canvas.height - z2.y - zY[1])
-        ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
-        ctx.lineTo(z3.x + zX[0], canvas.height - z3.y - zX[1])
-        ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
-        ctx.lineTo(z4.x + zZ[0], canvas.height - z4.y - zZ[1])
-        ctx.stroke()
+function cube(matrix, type = 1) {
+    if (type === 1) {
+        for (let i = 0; i < matrix.length; i++) {
+            let z1 = z(matrix[i])
+            ctx.lineWidth = 1
+            ctx.strokeStyle = 'blue'
+            const zY = validateY(matrix, i)
+            const z2 = z(zY)
+            const zX = validateX(matrix, i)
+            const z3 = z(zX)
+            const zZ = validateZ(matrix, i)
+            const z4 = z(zZ)
+            ctx.beginPath()
+            ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
+            ctx.lineTo(z2.x + zY[0], canvas.height - z2.y - zY[1])
+            ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
+            ctx.lineTo(z3.x + zX[0], canvas.height - z3.y - zX[1])
+            ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
+            ctx.lineTo(z4.x + zZ[0], canvas.height - z4.y - zZ[1])
+            ctx.stroke()
+        }
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            let z1 = z(matrix[i], type)
+            ctx.lineWidth = 1
+            ctx.strokeStyle = 'blue'
+            const zY = validateY(matrix, i)
+            const z2 = z(zY)
+            const zX = validateX(matrix, i)
+            const z3 = z(zX)
+            const zZ = validateZ(matrix, i)
+            const z4 = z(zZ)
+            ctx.beginPath()
+            ctx.moveTo(z1.x, canvas.height - z1.y)
+            ctx.lineTo(z2.x, canvas.height - z2.y)
+            ctx.moveTo(z1.x, canvas.height - z1.y)
+            ctx.lineTo(z3.x, canvas.height - z3.y)
+            ctx.moveTo(z1.x, canvas.height - z1.y)
+            ctx.lineTo(z4.x, canvas.height - z4.y)
+            ctx.stroke()
+        }
     }
 }
 
-function triangle(matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-        let z1 = z(matrix[i])
-        ctx.lineWidth = 1
-        ctx.strokeStyle = 'blue'
-        let zXZ = triangleValidate(matrix, i)
-        if (zXZ.length === 0) {
-            let zY = matrix.filter(el => el[1] < matrix[i][1])
-            zY.forEach(el => {
-                const z1 = z(matrix[i])
-                const z2 = z(el)
-                ctx.beginPath()
-                ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
-                ctx.lineTo(z2.x + el[0], canvas.height - z2.y - el[1])
-                ctx.stroke()
-            })
-        } else {
-            zXZ.forEach(el => {
-                const z2 = z(el)
-                ctx.beginPath()
-                ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
-                ctx.lineTo(z2.x + el[0], canvas.height - z2.y - el[1])
-                ctx.stroke()
-            })
+function triangle(matrix, type = 1) {
+    if (type === 1) {
+        for (let i = 0; i < matrix.length; i++) {
+            let z1 = z(matrix[i])
+            ctx.lineWidth = 1
+            ctx.strokeStyle = 'blue'
+            let zXZ = triangleValidate(matrix, i)
+            if (zXZ.length === 0) {
+                let zY = matrix.filter(el => el[1] < matrix[i][1])
+                zY.forEach(el => {
+                    const z1 = z(matrix[i])
+                    const z2 = z(el)
+                    ctx.beginPath()
+                    ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
+                    ctx.lineTo(z2.x + el[0], canvas.height - z2.y - el[1])
+                    ctx.stroke()
+                })
+            } else {
+                zXZ.forEach(el => {
+                    const z2 = z(el)
+                    ctx.beginPath()
+                    ctx.moveTo(z1.x + matrix[i][0], canvas.height - z1.y - matrix[i][1])
+                    ctx.lineTo(z2.x + el[0], canvas.height - z2.y - el[1])
+                    ctx.stroke()
+                })
+            }
+        }
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            let z1 = z(matrix[i], type)
+            ctx.lineWidth = 1
+            ctx.strokeStyle = 'blue'
+            let zXZ = triangleValidate(matrix, i)
+            if (zXZ.length === 0) {
+                let zY = matrix.filter(el => el[1] < matrix[i][1])
+                zY.forEach(el => {
+                    const z1 = z(matrix[i], type)
+                    const z2 = z(el, type)
+                    ctx.beginPath()
+                    ctx.moveTo(z1.x, canvas.height - z1.y)
+                    ctx.lineTo(z2.x, canvas.height - z2.y)
+                    ctx.stroke()
+                })
+            } else {
+                zXZ.forEach(el => {
+                    const z2 = z(el, type)
+                    ctx.beginPath()
+                    ctx.moveTo(z1.x, canvas.height - z1.y)
+                    ctx.lineTo(z2.x, canvas.height - z2.y)
+                    ctx.stroke()
+                })
+            }
         }
     }
 }
@@ -153,5 +207,5 @@ const matrix = [
     ],
 ]
 cube(matrix[0])
-cube(matrix[2])
-triangle(matrix[1])
+cube(matrix[2], 2)
+triangle(matrix[1], 2)
